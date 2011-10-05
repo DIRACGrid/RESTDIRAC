3fdb4dd5ae355af6f7da9fcf8427d68494fb4cdd
Adrià Casajús <adria@ecm.ub.es>
2011-10-05 16:43:13 +0200

    Added versioning to __init__.py

---
+++ __init__.py
@@ -0,0 +1,15 @@
+# Define Version
+
+majorVersion = 0
+minorVersion = 0
+patchLevel = 0
+preVersion = 1
+
+version = "v%sr%s" % ( majorVersion, minorVersion )
+buildVersion = "v%dr%d" % ( majorVersion, minorVersion )
+if patchLevel:
+  version = "%sp%s" % ( version, patchLevel )
+  buildVersion = "%s build %s" % ( buildVersion, patchLevel )
+if preVersion:
+  version = "%s-pre%s" % ( version, preVersion )
+  buildVersion = "%s pre %s" % ( buildVersion, preVersion )
