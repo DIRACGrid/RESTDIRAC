0978745db69343f533d7a71c32fe7eb02cab7ff5
Andrei Tsaregorodtsev <atsareg@in2p3.fr>
2013-06-05 22:16:09 +0200

    Added __RCSID__

--- RESTSystem/Service/OATokenStoreHandler.py
+++ RESTSystem/Service/OATokenStoreHandler.py
@@ -6,6 +6,8 @@ from DIRAC.Core.DISET.RequestHandler import RequestHandler
 from RESTDIRAC.RESTSystem.DB.OATokenDB import OATokenDB
 from RESTDIRAC.RESTSystem.Client import OAToken
 
+__RCSID__ = "$Id$"
+
 class OATokenStoreHandler( RequestHandler ):
 
   @classmethod
