8196fa05b68b3743dfa847d841f3e691df7b3e29
Andrei Tsaregorodtsev <atsareg@in2p3.fr>
2013-06-05 22:15:22 +0200

    Added missing setup parameter to generateToken call

--- RESTSystem/API/oa2/AuthHandler.py
+++ RESTSystem/API/oa2/AuthHandler.py
@@ -1,8 +1,13 @@
+################################################################################
+# $HeadURL $
+################################################################################
+"""
+  AuthHandler to provide Oauth authentication to the DIRAC REST server
+"""
 
-from tornado import web, gen
-from DIRAC import gConfig
+__RCSID__  = "$Id$"
+
 from RESTDIRAC.RESTSystem.Base.RESTHandler import RESTHandler, WErr, WOK
-from RESTDIRAC.RESTSystem.Client.OAToken import OAToken
 from DIRAC.ConfigurationSystem.Client.Helpers import Registry
 from RESTDIRAC.ConfigurationSystem.Client.Helpers.RESTConf import getCodeAuthURL
 
@@ -37,4 +42,52 @@ class AuthHandler( RESTHandler ):
       #ERROR!
       self.send_error( 400 )
 
+  def __getGroups( self, DN = False ):
+    if not DN:
+      credDict = self.getClientCredentials()
+      if not credDict:
+        return WErr( 401, "No certificate received to issue a token" )
+      DN = credDict[ 'subject' ]
+      if not credDict[ 'validDN' ]:
+        return WErr( 401, "Unknown DN %s" % DN )
+    result = Registry.getGroupsForDN( DN )
+    if not result[ 'OK' ]:
+      return WErr( 500, result[ 'Message' ] )
+    return WOK( { 'groups' : result[ 'Value' ] } )
 
+  def __clientCredentialsRequest( self ):
+    args = self.request.arguments
+    try:
+      group = args[ 'group' ][0]
+    except KeyError:
+      return WErr( 400, "Missing user group" )
+    try:
+      setup = args[ 'setup' ][0]
+    except KeyError:
+      return WErr( 400, "Missing setup" )
+    credDict = self.getClientCredentials()
+    if not credDict:
+      return WErr( 401, "No certificate received to issue a token" )
+    DN = credDict[ 'subject' ]
+    if not credDict[ 'validDN' ]:
+      return WErr( 401, "Unknown DN %s" % DN )
+    #Check group
+    result = self.__getGroups( DN )
+    if not result.ok:
+      return result
+    groups = result.data[ 'groups' ]
+    if group not in groups:
+      return WErr( 401, "Invalid group %s for %s (valid %s)" % ( group, DN, groups ) )
+    scope = False
+    if 'scope' in args:
+      scope = args[ 'scope' ]
+    result = self.__oaToken.generateToken( DN, group, setup, scope = scope, renewable = False )
+    if not result[ 'OK' ]:
+      return WErr( 500, "Error generating token: %s" % result[ 'Message' ] )
+    data = result[ 'Value' ][ 'Access' ]
+    res = {}
+    for ki, ko in ( ( "LifeTime", "expires_in" ), ( 'Token', 'token' ) ):
+      if ki in data:
+        res[ ko ] = data[ ki ]
+
+    return WOK( res )
