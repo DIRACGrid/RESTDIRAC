717a09962af19ebd7ec8006d0399eda77db85971
Andrei Tsaregorodtsev <atsareg@in2p3.fr>
2016-02-07 23:35:25 +0100

    New ProxyHandler class

---
+++ RESTSystem/API/Proxy/ProxyHandler.py
@@ -0,0 +1,86 @@
+""" Handler to manage proxies in the ProxyManager service
+"""
+
+__RCSID__ = "$Id$"
+
+import json
+from tornado import web, gen
+from RESTDIRAC.RESTSystem.Base.RESTHandler import WErr, WOK, RESTHandler
+from DIRAC.FrameworkSystem.Client.ProxyManagerClient import gProxyManager
+from DIRAC.Core.Utilities import Time
+from DIRAC.ConfigurationSystem.Client.Helpers.Registry import getDNForUsername, getUsernameForDN
+from DIRAC.Core.Security.X509Chain import X509Chain
+
+class ProxyHandler( RESTHandler ):
+
+  ROUTE = "/proxy/([a-z]+)/([a-z,_]+)"
+
+  def _getProxies( self, user, group ):
+
+    selDict = {}
+    if user.lower() != 'all':
+      selDict = { "UserName": user }
+
+    result = gProxyManager.getDBContents( selDict )
+    if not result['OK']:
+      return WErr( 500, result['Message'] )
+
+    records = result['Value']['Records']
+
+    resultList = []
+
+    for record in records:
+      if ( record[0] == user or user.lower() == 'all' ) and \
+         ( record[2] == group or group.lower() == 'all' ):
+        validity = Time.toString( record[3] )
+        resultList.append( { "User": record[0],
+                             "UserDN": record[1],
+                             "Group": record[2],
+                             "Validity": validity } )
+
+    return WOK( resultList )
+
+  @web.asynchronous
+  @gen.engine
+  def get( self, user, group ):
+
+    result = yield self.threadTask( self._getProxies, user, group )
+    if not result.ok:
+      raise result
+    data = result.data
+
+    self.finish( json.dumps( data ) )
+
+  def _uploadProxy( self, user, group, password, files ):
+
+    proxyChain = X509Chain()
+    proxyChain.loadChainFromString( files['cert'][0]['body'] )
+    proxyChain.loadKeyFromString( files['key'][0]['body'], password )
+    result = proxyChain.getIssuerCert()
+    if not result['OK']:
+      return WErr( 500, result['Message'] )
+    userDN = result['Value']
+    result = getUsernameForDN( userDN )
+    if not result['OK']:
+      return WErr( 500, result['Message'] )
+    if user != result['Value']:
+      return WErr( 500, 'User name does not correspond to certificate' )
+
+    result = gProxyManager.uploadProxy( proxyChain, group  )
+    if not result['OK']:
+      return WErr( 500, result['Message'] )
+
+    return WOK( result )
+
+  def post( self, user, group ):
+
+    result = getDNForUsername( user )
+    userDN = result['Value'][0]
+    files = self.request.files
+    password = self.request.arguments['Password'][0]
+
+    result = self._uploadProxy( user, group, password, files )
+    if not result.ok:
+      raise result
+    data = result.data
+    self.finish( { 'UploadedProxies': data['Value'][userDN].keys() } )
